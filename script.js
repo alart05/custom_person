2e9d1a8a8f4bafd6934e40b444b496949af5f595
alart05 <88630187+alart05@users.noreply.github.com>
2021-10-25 23:07:50 +0300

    new project

---
+++ script.js
@@ -0,0 +1,72 @@
+let circle1 = document.querySelector('.circle1')
+let circle2 = document.querySelector('.circle2')
+let circle3 = document.querySelector('.circle3')
+let circle4 = document.querySelector('.circle4')
+let circle5 = document.querySelector('.circle5')
+let eyes1 = document.querySelector('.eyes__img1')
+let eyes2 = document.querySelector('.eyes__img2')
+let eyes3 = document.querySelector('.eyes__img3')
+let eyes4 = document.querySelector('.eyes__img4')
+let eyes5 = document.querySelector('.eyes__img5')
+let finalEyes = document.querySelector('.column2__eyes')
+let hair1 = document.querySelector('.hair__img1')
+let hair2 = document.querySelector('.hair__img2')
+let hair3 = document.querySelector('.hair__img3')
+let hair4 = document.querySelector('.hair__img4')
+let hair5 = document.querySelector('.hair__img5')
+let finalHairs = document.querySelector('.column2__hair')
+let clothes1 = document.querySelector('.clothes__img1')
+let clothes2 = document.querySelector('.clothes__img2')
+let clothes3 = document.querySelector('.clothes__img3')
+let clothes4 = document.querySelector('.clothes__img4')
+let clothes5 = document.querySelector('.clothes__img5')
+let finalClothes = document.querySelector('.clothes')
+
+
+eyes1.addEventListener('click', () => {
+    finalEyes.src = 'eyes1.svg'
+})
+eyes2.addEventListener('click', () => {
+    finalEyes.src = 'eyes2.svg'
+})
+eyes3.addEventListener('click', () => {
+    finalEyes.src = 'eyes3.svg'
+})
+eyes4.addEventListener('click', () => {
+    finalEyes.src = 'eyes4.svg'
+})
+eyes5.addEventListener('click', () => {
+    finalEyes.src = 'eyes5.svg'
+})
+
+hair1.addEventListener('click', () => {
+    finalHairs.src = 'hair1.svg'
+})
+hair2.addEventListener('click', () => {
+    finalHairs.src = 'hair2.svg'
+})
+hair3.addEventListener('click', () => {
+    finalHairs.src = 'hair3.svg'
+})
+hair4.addEventListener('click', () => {
+    finalHairs.src = 'hair4.svg'
+})
+hair5.addEventListener('click', () => {
+    finalHairs.src = 'hair5.svg'
+})
+
+clothes1.addEventListener('click', () => {
+    finalClothes.src = 'clothes1.png'
+})
+clothes2.addEventListener('click', () => {
+    finalClothes.src = 'clothes2.png'
+})
+clothes3.addEventListener('click', () => {
+    finalClothes.src = 'clothes3.png'
+})
+clothes4.addEventListener('click', () => {
+    finalClothes.src = 'clothes4.png'
+})
+clothes5.addEventListener('click', () => {
+    finalClothes.src = 'clothes5.png'
+})
